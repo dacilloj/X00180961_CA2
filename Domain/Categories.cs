c2e858cf952f28677a8b1bd5de8f582b4a23d8c9
dacillo <johncarlodacillo123@gmail.com>
2022-12-05 18:38:26 +0000

    added domain

---
+++ Domain/Categories.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Domain
+{
+    public class Categories
+    {
+        public enum allowedCategoies
+        {
+            Football,
+            Hurling,
+            Clothes
+        }
+
+
+        public enum allowedSizes
+        {
+            XL,
+            X,
+            M,
+            S
+        }
+
+    }
+}
---
+++ Domain/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Drawing;
+using System.Text;
+
+namespace Domain
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        [Required]
+        //[StringLength()]
+        public string ProductName { get; set; } = ""; 
+
+        public double Price { get; set; }
+
+        [Range(0,5)]
+        public int Rating { get; set; }
+
+        public Categories category { get; set; }
+
+        public Sizes size { get; set; }
+    }
+}
